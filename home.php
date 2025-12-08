43fedc7d4db42db1fd8b8fd4f0ac0adc9b571116
Gabrielhj17 <gabrielhj@icloud.com>
2025-12-08 14:21:55 +0000

    Changed button spacing on home page

--- home.php
+++ home.php
@@ -13,11 +13,11 @@ if (!isset($_SESSION['user_id'])) {
     <link rel="stylesheet" href="styles.css">
     <title>LoveJoy Home</title>
 </head>
-<br>
+<body>
     <h1>Welcome, <?php echo htmlspecialchars($_SESSION['first_name']); ?>!</h1>
-    <a href="requestEval.php" class="button">Request Evaluation</a>
+    <a href="requestEval.php" class="button">Request Evaluation</a><br><br>
     <?php if (isset($_SESSION['is_admin']) && $_SESSION['is_admin'] == 1): ?>
-        <a href="admin.php" class="button">Admin Panel - View Requests</a>
+        <a href="admin.php" class="button">View Evaluation Requests</a><br><br>
     <?php endif; ?>
     <a href="logout.php" class="button">Logout</a>
 </body>
